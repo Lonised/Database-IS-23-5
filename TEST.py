f821345597dbf56f9ca14632bb8b21838d182d01
Daniil Shterkel <daniilshterkel05@gmail.com>
2024-11-11 14:31:53 +0500

    Тест4

---
+++ TEST.py
@@ -0,0 +1,30 @@
+#4 ВАРИАНТ
+
+Class Course:
+	def __init__ (self, _title, _credits):
+	self._title = _title
+	self._credits = _credits 
+	
+Class OnlineCourse(Course):
+	def __init__ (self, type=true)	
+
+	def start_video_lecture(
+		if type == true 
+		return ("Включить видео курс")
+	)
+
+Class OfflineCourse(Course):
+	def __init__ (self, type=false)
+
+	def schedule_classroom(
+		if type == false
+		return ("Срочно вернитесь в класс")  
+	)
+
+Class OutputOnlineCourse(Course)
+	get_course_info(self.title, self.credits, )
+	return (self.title, self.credits)
+
+Class OutputOfflineCourse(Course)
+	get_course_info(self.title, self.credits)
+	return (self.title, self.credits)
