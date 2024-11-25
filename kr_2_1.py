c75fd7448a714e16a6eec113b5df8b1c64ba77b3
Disafun <Nekif7777@gmail.com>
2024-11-25 15:33:44 +0600

    Varian1

---
+++ kr_2_1.py
@@ -0,0 +1,33 @@
+#Variant 1
+
+class University:
+    def __init__(self, university_name, info, year):
+        self.university_name = name
+        self.year = year
+        self.info = info
+
+class FacultyManager:
+    def __init__(self, faculty_name, dikan, num_of_students):
+        self.faculty = faculty
+        self.dikan = dikan
+        self.num_of_students = num_of_students
+
+    def add_faculty(self):
+        add self.faculty_name
+        print f("Добавлен факультет: ", {self.faculty_name})
+    return
+
+    def delete_faculty(self):
+        remove self.faculty_name
+        print f("Удален факультет", {self.faculty_name})
+    return
+    
+    def show_list(self, list_of_faculty):
+
+
+university = University("KarGU", "Good", 1967)
+faculty1 = FacultyManager("IS", "Maksim", 129)
+faculty2 = FacultyManager("IS", "Mark", 222)        
+        
+
+             
