c29c9364374737132467db43f9df97294a83ede4
ʟᴏɴɪꜱᴇᴅ <163004654+Lonised@users.noreply.github.com>
2024-11-28 11:24:05 +0500

    test_2_2

---
+++ kr_2_2.py
@@ -0,0 +1,47 @@
+class BankAccount:
+    def __init__ (self, __account_number, __balance):
+    self.__account_number = account_number
+    self.__balance = balance 
+    
+    def deposit(self):
+        return f"(пополнить)
+
+    def withdraw(self):
+        return f"(снять)
+
+	
+
+class Vehicle:
+    def __init__ (self, make, model, year):
+    self.make = make 
+    self.model = model 
+    self.year = year
+
+    get_info(self):
+	return (f"self.make self.model self.year)
+
+class Car(Vehicle):
+    def __init__ (number_of_doors)
+	self.number_of_doors = number_of_doors 
+
+class Truck(Vehicle):
+    def __init__ (payload_capacity)
+	self.payload_capacity = payload_capacity
+
+
+class Animal:
+    def __init__ (self):
+
+    
+class Dog(Animal):
+    def make_sound():
+	return(f"Woof!")
+ 
+class Cat(Animal):
+    def make_sound():
+	return(f"Meow!")
+
+class Bird(Animal):
+    def make_sound():
+	return(f"Chirp!")
+
